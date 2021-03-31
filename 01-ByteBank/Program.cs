4912be3313487981f82bb8bbc7fa6f2be0b7368f
bioformigoni <bioformigoni@gmail.com>
2021-03-30 21:04:15 -0300

    01 ByteBank

--- 01-ByteBank/Program.cs
+++ 01-ByteBank/Program.cs
@@ -10,6 +10,20 @@ namespace _01_ByteBank
     {
         static void Main(string[] args)
         {
+            new ContaCorrente();
+
+
+            string titular = "Gabriela";
+            int numeroAgencia = 863;
+            int numero = 863146;
+            double saldo = 100;
+
+            string titular2 = "Bruno";
+            int numeroAgencia2 = 863;
+            int numero2 = 863148;
+            double saldo2 = 50;
+
+            Console.ReadLine();
         }
     }
 }
