88e5bbd2555b141e57c58fd5def26b47eff94e69
bioformigoni <bioformigoni@gmail.com>
2021-04-12 18:43:43 -0300

    08 Composicao de classes

--- 05-ByteBank/Program.cs
+++ 05-ByteBank/Program.cs
@@ -11,6 +11,27 @@ namespace _05_ByteBank
         static void Main(string[] args)
         {
             Cliente gabriela = new Cliente();
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
         }
     }
 }
