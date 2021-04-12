347a14c4e17e742419e5c625426968ec1f400277
bioformigoni <bioformigoni@gmail.com>
2021-04-12 14:54:41 -0300

    04 Primeira funcao

---
+++ 04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+    
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor) // "this" indica uma váriável do escopo da própria classe
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+}
