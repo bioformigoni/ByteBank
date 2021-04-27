ef929320d6099195e5a867cda8f6c4a3ca8a50c8
bioformigoni <bioformigoni@gmail.com>
2021-04-27 20:53:58 -0300

    10 ObterSaldo e DefinirSaldo

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,62 @@
+﻿namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        private double saldo = 100;
+
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0) 
+            {
+                return;
+            }
+            
+            this.saldo = saldo;
+        }
+
+        public double ObterSaldo() 
+        {
+            return saldo;
+        }
+
+        // Tem valor de retorno = função
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor) // "this" indica uma váriável do escopo da própria classe
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        // Não tem valor de retorno = método
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+
+        }
+    }
+}
